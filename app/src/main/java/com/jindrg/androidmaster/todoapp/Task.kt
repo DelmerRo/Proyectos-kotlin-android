a51f7ab45e4570b4d02c97f7eb23349c18951731
Jindrg <jindrg@gmail.com>
2023-04-05 08:43:48 -0300

    Agregando Nueva Aplicacion(TodoAPP)

---
+++ app/src/main/java/com/jindrg/androidmaster/todoapp/Task.kt
@@ -0,0 +1,5 @@
+package com.jindrg.androidmaster.todoapp
+
+data class Task(val name:String, val category: TaskCategory, var isSelected:Boolean=false) {
+
+}
--- app/src/main/java/com/jindrg/androidmaster/todoapp/TodoActivity.kt
+++ app/src/main/java/com/jindrg/androidmaster/todoapp/TodoActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.jindrg.androidmaster.R
+import com.jindrg.androidmaster.mutableList
 
 class TodoActivity : AppCompatActivity() {
 
@@ -14,8 +15,17 @@ class TodoActivity : AppCompatActivity() {
         TaskCategory.Other
     )
 
+    private val tasks = mutableListOf(
+        Task("PruebaBusiness", TaskCategory.Business),
+        Task("PruebaPersonal", TaskCategory.Personal),
+        Task("PruebaOther", TaskCategory.Other)
+    )
+
     private lateinit var rvCategories: RecyclerView
     private lateinit var categoriesAdapter: CategoriesAdapter
+
+    private lateinit var rvTasks: RecyclerView
+    private lateinit var tasksAdapter: TasksAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +36,7 @@ class TodoActivity : AppCompatActivity() {
 
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
+        rvTasks = findViewById(R.id.rvTasks)
     }
 
 
@@ -33,6 +44,10 @@ class TodoActivity : AppCompatActivity() {
         categoriesAdapter = CategoriesAdapter(categories)
         rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
     }
 
 }
